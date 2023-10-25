eb94e6932b9cddb035a30a1f997fbe7bae58c441
HugoSanche <hbmclaren@gmail.com>
2023-10-24 22:51:14 -0600

    Rest Exception handling

--- src/main/java/com/individual/rest/IndividualRestController.java
+++ src/main/java/com/individual/rest/IndividualRestController.java
@@ -2,10 +2,9 @@ package com.individual.rest;
 
 import com.individual.entity.Individual;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -35,6 +34,64 @@ public class IndividualRestController {
     }
     @GetMapping("individual/{id}")
     public Individual getTheIndividualbyID(@PathVariable int id){
+
+
+        // check if the individual be en the list size
+        if (id>=theIndividual.size() || id<0 ){
+                throw new IndividualNotFoundException("Individual id not found - "+id);
+        }
         return theIndividual.get(id);
+
+    }
+    // Add an exception handler using @ExceptionHandler
+
+
+    //Help to catch error and show the error en the browser  ExceptionHandler*//////
+
+
+    @ExceptionHandler
+    public ResponseEntity<IndividualErrorResponse> handleException (IndividualNotFoundException exc){
+        // create a StudentErrorResponse
+
+        IndividualErrorResponse error =new IndividualErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    // add another exception handler .. to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<IndividualErrorResponse> handleException (Exception exc){
+        // create a StudentErrorResponse
+
+        IndividualErrorResponse error =new IndividualErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
